ff8428de7d0a0ef3da3cade908a0537a4a79628e
Kunal Mathur <mathurkunal@gmail.com>
2020-04-18 01:31:51 -0400

    updating test files

---
+++ src/test/java/data/DataApplicationTests.java
@@ -0,0 +1,11 @@
+package data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
